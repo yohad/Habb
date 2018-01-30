8eff3ec0652a2d4af6d632ba710d8b37f2740c2b
Yotam Ohad <yotam2206@gmail.com>
2018-01-30 11:04:17 +0000

    Found a magic elf that told me the right nix packages

--- app/Main.hs
+++ app/Main.hs
@@ -1,14 +1,4 @@
 module Main where
 
-import Graphics.UI.WX
-import Lib
-
 main :: IO ()
-main =
-  start hello
-
-hello :: IO ()
-hello = do
-  f <- frame [text := "Hello!"]
-  quit <- button f [text := "Quit", on command := close f]
-  set f [layout := widget quit]
+main = print 1234
